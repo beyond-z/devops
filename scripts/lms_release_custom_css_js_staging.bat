3bf6bf1747a9f914d5796017cd2376369a466d64
Admin Server <admin@bebraven.org>
2016-05-11 19:59:02 +0000

    Commented out attempt at making custom CSS/JS changes take effect without updating settings in UI

--- scripts/lms_release_custom_css_js_staging.bat
+++ scripts/lms_release_custom_css_js_staging.bat
@@ -18,6 +18,16 @@ if aws --version 2> /dev/null; then
     exit 1
   fi
 
+  # TODO: doesn't work.  The updated value shows up in the UI, but it doesn't take effect.  
+  # Maybe the settings are only read from the DB on server load.  I tried rebooting the server and it didn't work either....
+  # Commenting out for now.
+
+  #echo "Updating Custom CSS/JS settings on staging to invalidate browser cache"
+  ## The below sed find/replace updates the version query string in the settings for both bz_custom.js and bz_custom.css
+  #now=$(date +"%Y%m%d_%H%M")
+  #current_settings=`psql -h $PORTAL_STAGING_DB_SERVER -p 5432 -U $PORTAL_PROD_DB_USER -d $PORTAL_PROD_DB_NAME -w -t -A -c "select settings from accounts where id = 1;"`
+  #echo "update accounts set settings = '$current_settings' where id = 1" | sed -e "s/bz_custom\.js?v=[_0-9]*/bz_custom.js?v=$now/g;s/bz_custom\.css?v=[_0-9]*/bz_custom.css?v=$now/g;" | psql -h $PORTAL_STAGING_DB_SERVER -p 5432 -U $PORTAL_PROD_DB_USER -d $PORTAL_PROD_DB_NAME -w
+  
 else
   # Install AWS CLI if it's not there
   echo "Error: Please install 'aws'. E.g."
