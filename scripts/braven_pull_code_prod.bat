468a559d8a517921de8348e7e004be913a1454eb
Admin Server <admin@bebraven.org>
2019-08-13 13:31:26 +0000

    Update script to pull code from prod public website.

--- scripts/braven_pull_code_prod.bat
+++ scripts/braven_pull_code_prod.bat
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.env
 
-cd ~/src/braven
+cd ~/src/braven_2
 git checkout master 
 git pull origin master
-rsync -avz --exclude 'wp-content/updraft' --exclude 'wp-content/cache' --exclude '.git' $BRAVEN_PROD_USER:/var/www/ ~/src/braven
+rsync -avz --exclude '**tar.gz**' --exclude 'wp-content/updraft' --exclude 'wp-content/updraftplus' --exclude 'wp-content/cache' --exclude '.git' $BRAVEN_PROD_USER:/var/www/html/ ~/src/braven_2
