b6345533ac4213adef002ebfc5dc2bf609f8e4f9
Admin Server <admin@bebraven.org>
2020-02-05 16:01:21 +0000

    Update LMS backup/snapshot script to use the Heroku hosted DB.

--- scripts/backup_scripts/lms_create_snapshot.sh
+++ scripts/backup_scripts/lms_create_snapshot.sh
@@ -11,10 +11,19 @@ fi
 # Maybe I can clear the main values in those columns in those tables when i transfer?
 
 dump_file=$1
+prod_dump_file=${dump_file}.prod.dump
 
 echo "### Snapshotting the Canvas production database and migrating it to a staging database"
 
-# Remove s3:// prefix since we're targeting a URL like: https://s3.amazonaws.com/<bucket_name>/<file_name>
+echo "Preparing for download of snapshot"
+heroku pg:backups capture --app $PORTAL_PROD_APP \
+  || { echo >&2 "Error: command 'heroku pg:backups capture --app $PORTAL_PROD_APP' failed."; exit 1; }
+# Note: this is in pg_restore compressed format.
+echo "Downloading snapshot to $dump_file"
+heroku pg:backups:download -o $prod_dump_file --app $PORTAL_PROD_APP \
+  || { echo >&2 "Error: command 'heroku pg:backups:download -o $dump_file --app $PORTAL_PROD_APP' failed."; exit 1; }
+
+## Remove s3:// prefix since we're targeting a URL like: https://s3.amazonaws.com/<bucket_name>/<file_name>
 escaped_prod_bucket=${PORTAL_S3_PROD_BUCKET//s3:\/\/}
 escaped_staging_bucket=${PORTAL_S3_STAGING_BUCKET//s3:\/\/}
 
@@ -22,15 +31,19 @@ escaped_staging_bucket=${PORTAL_S3_STAGING_BUCKET//s3:\/\/}
 #escaped_prod_bucket=${PORTAL_S3_PROD_BUCKET//\//\\/}
 #escaped_staging_bucket=${PORTAL_S3_STAGING_BUCKET//\//\\/}
 
-pg_dump --clean -h $PORTAL_PROD_DB_SERVER -p 5432 -U $PORTAL_PROD_DB_USER -w -d $PORTAL_PROD_DB_NAME | sed -e "
+# Turns compressed DB into plain sql, pipes that to sed to do some regex replaces, 
+# then creates a plain old gzip of the sql commands.
+pg_restore $prod_dump_file | sed -e "
   # Replace production access token with staging token. Encrypted values
   # gotten from the two current dumps and just replaced here.
   $PORTAL_REPLACE_PROD_ACCESS_TOKEN_WITH_STAGING_REGEX
   # this is the access token hint
   $PORTAL_REPLACE_PROD_ACCESS_TOKEN_HINT_WITH_STAGING_REGEX
 
-  # SSO config
+  # SSO config - OLD but doesnt hurt
   s/sso.bebraven.org/stagingsso.bebraven.org/g;
+  # New SSO config / platform
+  s/platform.bebraven.org/stagingplatform.bebraven.org/g;
   # Main site
   s/join.bebraven.org/stagingjoin.bebraven.org/g;
   # Also fix up internal links in assignments to stay on staging as we navigate
@@ -47,12 +60,10 @@ pg_dump --clean -h $PORTAL_PROD_DB_SERVER -p 5432 -U $PORTAL_PROD_DB_USER -w -d 
 if [ $? -ne 0 ]
 then
   echo "Failed creating Canvas staging database from production."
-  echo ""
-  echo "Double check that you have a ~/.pgpass file with credentials to connect to the staging and prod databases. Note that the ~/.pgpass file should have permissions set to chmod 600.  Example of file contents:"
-  echo "$PORTAL_STAGING_DB_SERVER:5432:*:$PORTAL_PROD_DB_USER:yourPass"
-  echo "$PORTAL_PROD_DB_SERVER:5432:*:$PORTAL_PROD_DB_USER:yourPass"
   exit 1;
 fi
+
+rm $prod_dump_file
 
 echo "### Done: Snapshotting the Canvas production database and migrating it to a staging database"
 
